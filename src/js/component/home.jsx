a977ee944d4c03d27bc6f5487d1b3aac582201dc
Marcello-Marques <137443029+Marcello-Marques@users.noreply.github.com>
2023-09-04 20:30:46 +0000

    Empyte string on Enter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,51 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
+	const [input,setInput] = useState("")
+	const [list,setList] = useState([])
+	const[count,setCount]= useState(0);
+	const deleteItem = () => {	
+	}
+useEffect (()=>{setCount(list.length);},[list]);
+	const countTask = count < 2 ? 'Task' : 'Tasks';
+
+		return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>4Geeks: To Do List</h1>
+			<div className="inputTasks">
+				<input	type="text"
+						onKeyDown={(event)=>{
+						if (event.keyCode == 13){
+							setList([...list,input])
+							setInput("");
+						}
+					}
+				}
+				value={input}
+						onChange={(e)=> setInput(e.target.value)}
+						placeholder="What do I need to do?">
+				</input>
+			</div>
+
+			{list.length > 0 ? list.map((task,index)=>{
+				return( 
+				<div className="rowTask">
+					<div>
+						<ul>
+						<li>{task}</li>
+						</ul>
+					</div>
+					<div onClick={()=>deleteItem()} className= "iconDelete">
+						<i class="fas fa-trash-alt"></i>
+					</div>
+				</div>
+				);
+			}):""}
+				<div>{count} {countTask}</div>
 		</div>
 	);
 };
